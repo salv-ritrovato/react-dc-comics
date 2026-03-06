328b9c337f11d7b840fbf1abce83868f08ee0cfe
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-06 18:29:38 +0100

    online shop almost completed, will go back to it later

--- src/components/OnlineShop.jsx
+++ src/components/OnlineShop.jsx
@@ -7,18 +7,34 @@ import powerVisa from '../../src/assets/img/buy-dc-power-visa.svg'
 export default function AppHeader() {
     return (
         <>
-        <div className="onlineshop">
-            <img src={eComics} alt="Digital Comics" />
-            <span>DIGITAL COMICS</span>
-            <img src={merchandise} alt="DC Merchandise" />
-            <span>DC MERCHANDISE</span>
-            <img src={subscription} alt="Monthly Subscription" />
-            <span>SUBSCRIPTION</span>
-            <img src={shopLocator} alt="Find Your Shop" />
-            <span>COMIC SHOP LOCATOR</span>
-            <img src={powerVisa} calt="DC Power Visa" />
-            <span>DC POWER VISA</span>
-        </div>
+            <div className="container-fluid onlineshop">
+                <div className="row text-center g-5">
+                    <div className="col-12 col-md-4 col-lg">
+                        <img src={eComics} alt="Digital Comics" className="img-fluid" />
+                        <span className='d-none d-lg-inline'>DIGITAL COMICS</span>
+                    </div>
+
+                    <div className="col-12 col-md-4 col-lg">
+                        <img src={merchandise} alt="DC Merchandise" className="img-fluid" />
+                        <span className='d-none d-lg-inline'>DC MERCHANDISE</span>
+                    </div>
+
+                    <div className="col-12 col-md-4 col-lg">
+                        <img src={subscription} alt="Monthly Subscription" className="img-fluid" />
+                        <span className='d-none d-lg-inline'>SUBSCRIPTION</span>
+                    </div>
+
+                    <div className="col-12 col-md-4 col-lg">
+                        <img src={shopLocator} alt="Find Your Shop" className="img-fluid" />
+                        <span className='d-none d-lg-inline'>COMIC SHOP LOCATOR</span>
+                    </div>
+
+                    <div className="col-12 col-md-4 col-lg">
+                        <img src={powerVisa} alt="DC Power Visa" className="img-fluid" />
+                        <span className='d-none d-lg-inline'>DC POWER VISA</span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
