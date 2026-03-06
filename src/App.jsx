ee25274ffb256b1cb3eacb486848f63d154670f2
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-06 12:47:56 +0100

    imported header, main and footer into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
 
 function App() {
 
   return (
     <>
-
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </>
   )
 }
