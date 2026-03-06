42a2c5f507536ecf54cbac99dae483806483928e
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-06 15:32:21 +0100

    main content completed

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,5 +1,8 @@
 export default function AppMain() {
    return <main>
-    
+      <div className="content">
+         {/* Using an HTML entity I found online in order to replicate the arrows */}
+         <p className=""> --&gt; Content goes here &lt;-- </p>
+      </div>
    </main>
 }
