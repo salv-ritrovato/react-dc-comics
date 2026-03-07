30d6fbffd8a2bbd003c45a9649472c5effbe0900
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-07 07:36:59 +0100

    fixed header responsiveness by reworking bootstrap classes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,26 +4,26 @@ export default function AppHeader() {
     return (
         <>
             <header>
-                <a href=""><img src={dcLogo} alt="DC Comics Logo" /></a>
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <div className="container-fluid">
+                    <div className="container-fluid d-flex justify-content-between align-items-center">
+                        <a className="navbar-brand" href="">
+                            <img src={dcLogo} alt="DC Comics Logo" />
+                        </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a href="">CHARACTERS</a>
-                                    <a href="">COMICS</a>
-                                    <a href="">MOVIES</a>
-                                    <a href="">TV</a>
-                                    <a href="">GAMES</a>
-                                    <a href="">COLLECTIBLES</a>
-                                    <a href="">VIDEOS</a>
-                                    <a href="">FANS</a>
-                                    <a href="">NEWS</a>
-                                    <a href="">SHOP</a>
-                                </li>
+                        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul className="navbar-nav mb-2 mb-lg-0">
+                                <li className="nav-item"><a className="nav-link" href="">CHARACTERS</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">COMICS</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">MOVIES</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">TV</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">GAMES</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">COLLECTIBLES</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">VIDEOS</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">FANS</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">NEWS</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">SHOP</a></li>
                             </ul>
                         </div>
                     </div>
